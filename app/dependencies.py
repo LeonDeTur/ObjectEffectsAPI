2cb6d3a7f590520b3c2a01e6d06effbcfdc551c4
Leon <100690100+LeonDeTur@users.noreply.github.com>
2025-02-10 18:24:38 +0300

    feat(app): - created app structure - added app instance - added effects router instance

---
+++ app/dependencies.py
@@ -0,0 +1,28 @@
+import sys
+from datetime import datetime
+
+from loguru import logger
+from iduconfig import Config
+
+from app.common.exceptions.http_exception_wrapper import http_exception
+from app.common.api_handler.api_handler import APIHandler
+
+
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+log_level = "INFO"
+log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <b>{message}</b>"
+logger.add(
+    sys.stderr,
+    format=log_format,
+    level=log_level,
+    colorize=True
+)
+
+config = Config()
+
+logger.add(
+    f"{config.get('LOGS_FILE')}.log",
+    format=log_format,
+    level="INFO",
+)
---
+++ app/main.py
@@ -0,0 +1,33 @@
+import aiofiles
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .dependencies import config
+
+
+app = FastAPI(
+    title="ObjectNat effects API",
+    description="API for calculating effects for territory by ObjectNat library",
+    version="0.0.1",
+)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/status")
+async def read_root():
+    return {"status": "OK"}
+
+@app.get("/logs")
+async def read_logs():
+    async with aiofiles.open(config.get("LOGS_FILE")) as logs_file:
+        logs = await logs_file.read()
+        return logs[-1:-10000]
+        
