091e2ad0e6be33ab3fa9ff28c9b49844819e6005
Leon <100690100+LeonDeTur@users.noreply.github.com>
2025-02-14 18:50:43 +0300

    feat(objectnat_Calculator): - started provision calculator

--- app/effects/modules/objectnat_calculator.py
+++ app/effects/modules/objectnat_calculator.py
@@ -1,31 +1,56 @@
 from typing import Literal
 
+import pandas as pd
 import geopandas as gpd
-from objectnat import 
+from objectnat import get_service_provision
 
 
 class ObjectNatCalculator:
 
     @staticmethod
+    def evaluate_provision(
+            buildings: gpd.GeoDataFrame,
+            services: gpd.GeoDataFrame,
+            matrix: pd.DataFrame,
+            service_normative: int
+    ) -> dict[str, gpd.GeoDataFrame]:
+        """
+        Function calculates provision and writes results as dict with fields "buildings", "services" and "links"
+        Args:
+            buildings (gpd.GeoDataFrame): GeoDataFrame of buildings
+            services (gpd.GeoDataFrame): GeoDataFrame of services
+            matrix (pd.DataFrame): GeoDataFrame of matrix
+            service_normative (int): service normative accessibility
+        Returns:
+            dict[str, gpd.GeoDataFrame]: dict with fields "buildings", "services" and "links"
+        """
+
+        build_prov, services_prov, links_prov = get_service_provision(
+            buildings=buildings,
+            services=services,
+            adjacency_matrix=matrix,
+            threshold=service_normative,
+        )
+
+        return {
+            "buildings": buildings,
+            "services": services,
+            "links": links_prov,
+        }
+
+    @staticmethod
     def estimate_effects(
-            buildings_before: gpd.GeoDataFrame,
-            services_before: gpd.GeoDataFrame,
-            buildings_after: gpd.GeoDataFrame,
-            services_after: gpd.GeoDataFrame,
-            normative_value: float,
-            normative_type: Literal["time", "dist"]
+            provision_before: gpd.GeoDataFrame,
+            provision_after:gpd.GeoDataFrame,
     ) -> gpd.GeoDataFrame:
         """
         Main function which calculates provision and estimates effects
         Args:
-            buildings_before (gpd.GeoDataFrame): layer with buildings before effects
-            services_before: (gpd.GeoDataFrame): layer with services before effects
-            buildings_after: (gpd.GeoDataFrame): layer with buildings after effects
-            services_after: (gpd.GeoDataFrame): layer with services after effects
-            normative_value: (int): normative time or dist to cut
-            normative_type: (Literal["time", "dist"]): normative type
+            provision_before (gpd.GeoDataFrame): Provision before
+            provision_after (gpd.GeoDataFrame): Provision after
         Returns:
             gpd.GeoDataFrame: layer with effects, provision before and after attributes
         """
 
-        provision_before,
+
+
