a52dbf7ec96e2a2e372f73d052d9acfcdfc13b8b
Leon <turkov.leon-2@yandex.ru>
2025-02-17 04:06:35 +0300

    feat(dto, schema): - dto for input-output schema

--- app/effects/dto/effects_dto.py
+++ app/effects/dto/effects_dto.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
@@ -5,3 +7,13 @@ class EffectsDTO(BaseModel):
 
     project_id: int = Field(..., examples=[72], description="Project ID")
     scenario_id: int = Field(..., examples=[192], description="Scenario ID")
+    service_type_id: int = Field(..., examples=[7], description="Service type ID")
+    year: Optional[int] = Field(
+        default=2024,
+        examples=[2024],
+        description="Year for data retrieval")
+    target_population: Optional[int] = Field(
+        default=None,
+        examples=[200],
+        description="Target population for project territory"
+    )
--- app/effects/shemas/effects_base_schema.py
+++ app/effects/shemas/effects_base_schema.py
@@ -1,16 +1,37 @@
-from typing import Literal
+from typing import Literal, Optional, Any
 
 from pydantic import BaseModel
 
 
 class GeometrySchema(BaseModel):
 
-    type: Literal["Polygon"]
-    coordinates: list[list[list[float | int]]]
+    type: Literal["Polygon", "MultiPolygon", "LineString", "MultiLineString", "Point", "MultiPoint"]
+    coordinates: list[Any]
 
 
 class FeatureSchema(BaseModel):
 
+    id: Optional[int | None]
     type: Literal["Feature"]
     geometry: GeometrySchema
+    properties: dict
 
+
+class FeatureCollectionSchema(BaseModel):
+
+    type: Literal["FeatureCollection"]
+    features: list[FeatureSchema]
+
+
+class ProvisionSchema(BaseModel):
+
+    provision: FeatureCollectionSchema
+    services: FeatureCollectionSchema
+    links: FeatureCollectionSchema
+
+
+class EffectsSchema(BaseModel):
+
+    provision_before: ProvisionSchema
+    provision_after: ProvisionSchema
+    effects: FeatureCollectionSchema
