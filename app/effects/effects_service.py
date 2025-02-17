bb066b6451d6125031c2d02b439469ff3d128f6a
Leon <turkov.leon-2@yandex.ru>
2025-02-17 04:07:57 +0300

    feat(effects_service): - finished effects service for api

--- app/effects/effects_service.py
+++ app/effects/effects_service.py
@@ -1,4 +1,16 @@
+import json
+import asyncio
+
+import geopandas as gpd
+import pandas as pd
+
 from .dto.effects_dto import EffectsDTO
+from .modules import (
+    effects_api_gateway,
+    data_restorator,
+    attribute_parser,
+    matrix_builder, objectnat_calculator
+)
 
 
 class EffectsService:
@@ -6,14 +18,93 @@ class EffectsService:
     Class for handling services calculation
     """
 
+    # ToDo Make in and out crs convertations here
+    # Todo Ensure async
     @staticmethod
-    async def calculate_effects(params: EffectsDTO) -> dict:
+    async def calculate_effects(effects_params: EffectsDTO) -> dict[str, dict]:
         """
         Calculate provision effects by project data and target scenario
         Args:
-            params (EffectsDTO): Project data
+            effects_params (EffectsDTO): Project data
         Returns:
-             dict: Provision effects
+             gpd.GeoDataFrame: Provision effects
         """
 
-        pass
+        project_data = await effects_api_gateway.get_project_data(
+            effects_params.project_id
+        )
+        normative_data = await effects_api_gateway.get_service_normative(
+            territory_id=project_data["territory"]["id"],
+            service_type_id=effects_params.service_type_id,
+            year=effects_params.year,
+        )
+        context_buildings = await effects_api_gateway.get_scenario_buildings(
+            scenario_id=project_data["project"]["base_scenario"]["id"]
+        )
+        context_buildings = await attribute_parser.parse_living_area_from_buildings(
+            living_buildings=context_buildings,
+        )
+        context_services = await effects_api_gateway.get_scenario_services(
+            scenario_id=project_data["project"]["base_scenario"]["id"]
+        )
+        project_buildings = await effects_api_gateway.get_scenario_buildings(
+            scenario_id=effects_params.scenario_id
+        )
+        project_services = await effects_api_gateway.get_scenario_services(
+            scenario_id=effects_params.scenario_id
+        )
+        after_buildings = await asyncio.to_thread(
+            pd.concat,
+            [context_buildings, project_buildings]
+        )
+        after_services = await asyncio.to_thread(
+            pd.concat,
+            [context_services, project_services]
+        )
+        before_matrix = await asyncio.to_thread(
+            matrix_builder.calculate_availability_matrix,
+            buildings=context_buildings,
+            context_services=context_services,
+            normative_value=normative_data["normative_value"],
+            normative_type=normative_data["normative_type"],
+        )
+        after_matrix = await asyncio.to_thread(
+            matrix_builder.calculate_availability_matrix,
+            buildings=after_buildings,
+            context_services=context_services,
+            normative_value=normative_data["normative_value"],
+            normative_type=normative_data["normative_type"],
+        )
+        before_prove_data = objectnat_calculator.evaluate_provision(
+            buildings=context_buildings,
+            services=context_services,
+            matrix=before_matrix,
+            service_normative=normative_data["service_normative"],
+        )
+        after_prove_data = objectnat_calculator.evaluate_provision(
+            buildings=after_buildings,
+            services=after_services,
+            matrix=after_matrix,
+            service_normative=normative_data["service_normative"],
+        )
+        effects = objectnat_calculator.estimate_effects(
+            before_prove_data=before_prove_data["provision"],
+            after_prove_data=after_prove_data["provision"],
+        )
+        result = {
+            "before_prove_data": {
+                "provision": json.load(before_prove_data["provision"]),
+                "services": json.load(before_prove_data["services"]),
+                "links": json.load(before_prove_data["links"]),
+            },
+            "after_prove_data": {
+                "provision": json.load(after_prove_data["provision"]),
+                "services": json.load(after_prove_data["services"]),
+                "links": json.load(after_prove_data["links"]),
+            },
+            "effects": json.load(effects.json()),
+        }
+        return result
+
+
+effects_service = EffectsService()
---
+++ app/effects/modules/__init__.py
@@ -0,0 +1,5 @@
+from .attribute_parser import attribute_parser
+from .effects_api_gateway import effects_api_gateway
+from .demand_restorator import data_restorator
+from .matrix_builder import matrix_builder
+from .objectnat_calculator import objectnat_calculator
