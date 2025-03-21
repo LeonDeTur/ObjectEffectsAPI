150372fa11c0516e8dfee3b03d1295bd4307739e
Leon <100690100+LeonDeTur@users.noreply.github.com>
2025-03-21 18:20:51 +0300

    fix(provision_calculator): - absolute mark bug resolved finally

--- app/effects/modules/objectnat_calculator.py
+++ app/effects/modules/objectnat_calculator.py
@@ -111,27 +111,39 @@ class ObjectNatCalculator:
         supplied_demand_within_after = effects["supplyed_demands_within_after"].fillna(0)
         supplied_demand_without_after = effects["supplyed_demands_without_after"].fillna(0)
         unsupplied_demand_within_before = effects["us_demands_within_before"].fillna(0)
-        unsupplied_demand_without_before = effects["us_demands_without_before"].fillna(0)
         unsupplied_demand_within_after = effects["us_demands_within_after"].fillna(0)
-        unsupplied_demand_without_after = effects["us_demands_without_after"].fillna(0)
         total_supplied_demands_before = supplied_demand_without_before + supplied_demand_within_before
         total_supplied_demands_after = supplied_demand_without_after + supplied_demand_within_after
-        total_us_demands_before = unsupplied_demand_without_before + unsupplied_demand_within_before
-        total_us_demands_after = unsupplied_demand_without_after + unsupplied_demand_within_after
+        total_us_demands_before = effects["demand"] - total_supplied_demands_before
+        total_us_demands_after = effects["demand"] - total_supplied_demands_after
         total_demand = int(effects["demand"].sum())
+
         project_total_supplied_demands_before = effects[
             effects["is_project"]
-        ]["supplyed_demands_without_before"].fillna(0)
+        ]["supplyed_demands_without_before"].fillna(0) + effects[
+            effects["is_project"]
+        ]["supplyed_demands_within_before"].fillna(0)
+
         project_total_supplied_demands_after = effects[
             effects["is_project"]
-        ]["supplyed_demands_without_after"].fillna(0)
+        ]["supplyed_demands_without_after"].fillna(0) + effects[
+            effects["is_project"]
+        ]["supplyed_demands_within_after"].fillna(0)
+
         project_total_us_demands_before = effects[
             effects["is_project"]
-        ]["us_demands_without_before"].fillna(0)
+        ]["demand"].fillna(0) - effects[
+            effects["is_project"]
+        ]["supplyed_demands_within_before"].fillna(0)
+
         project_total_us_demands_after = effects[
             effects["is_project"]
-        ]["us_demands_without_after"].fillna(0)
+        ]["demand"].fillna(0) - effects[
+            effects["is_project"]
+        ]["supplyed_demands_within_after"].fillna(0)
+
         project_total_demand = int(effects[effects["is_project"]]["demand"].sum())
+
         effects["absolute_total"] = self._calculate_absolute(
             supplied_demand_after=total_supplied_demands_after,
             supplied_demand_before=total_supplied_demands_before,
@@ -163,12 +175,6 @@ class ObjectNatCalculator:
             supplied_demand_after=supplied_demand_within_after,
             unsupplied_demand_before=unsupplied_demand_within_before,
             unsupplied_demand_after=unsupplied_demand_within_after
-        )
-        effects["absolute_without"] = self._calculate_absolute(
-            supplied_demand_before=supplied_demand_without_before,
-            supplied_demand_after=supplied_demand_without_after,
-            unsupplied_demand_before=unsupplied_demand_without_before,
-            unsupplied_demand_after=unsupplied_demand_without_after
         )
         return effects
 
@@ -237,7 +243,6 @@ class ObjectNatCalculator:
                 "absolute_scenario_project",
                 "index_scenario_project",
                 "absolute_within",
-                "absolute_without",
                 "demand",
                 "is_project"
             ]
